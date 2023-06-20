1275e4e7782e65a833bf9c857a843cdbfff7bd1c
Renato Athaydes <renato@athaydes.com>
2023-06-20 21:05:17 +0200

    Refactored tests using macro to simplify test declarations.

--- tests/parse-tests.lisp
+++ tests/parse-tests.lisp
@@ -4,57 +4,53 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :lisp-site-gen)' in your Lisp.
 
+(defmacro test-md (name lines expected)
+  `(testing ,name
+    (let ((output nil))
+      (md-parser:parse-markdown-lines ,lines (lambda (o) (push o output)))
+      (ok (equal (reverse output) ,expected)))))
+
 (deftest parse-markdown-text
 
-  (testing "can parse plain text"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("simple line" "other text") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:span "simple line") (:span "other text"))))))
+  (test-md "can parse plain text"
+           '("simple line" "other text")
+           '((:span "simple line") (:span "other text")))
 
-  (testing "can parse header 1"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("# hello") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:h1 "hello"))))))
+  (test-md "can parse header 1"
+           '("# hello")
+           '((:h1 "hello")))
 
-  (testing "can parse header 2"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("## hello") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:h2 "hello"))))))
+  (test-md "can parse header 2"
+           '("## hello")
+           '((:h2 "hello")))
 
-  (testing "can parse header 3"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("### header 3") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:h3 "header 3"))))))
+  (test-md "can parse header 3"
+           '("### header 3")
+           '((:h3 "header 3")))
 
-  (testing "can parse header 7"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("####### 7") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:h7 "7"))))))
+  (test-md "can parse header 7"
+           '("####### 7")
+           '((:h7 "7")))
 
-  (testing "can parse simple quote"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("> a quote") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:quotation " a quote"))))))
+  (test-md "can parse simple quote"
+           '("> a quote")
+           '((:quotation " a quote")))
 
-  (testing "can parse multi-line quote"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '(">start" "end") (lambda (o) (push o output)))
-      (ok (equal (reverse output) `((:quotation ,(format nil "start~%end")))))))
+  (test-md "can parse multi-line quote"
+           '(">start" "end")
+           `((:quotation ,(format nil "start~%end"))))
 
-  (testing "can parse text with emphasis"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("hello *world*!") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:span "hello " (:em "world") "!"))))))
+  (test-md "can parse text with emphasis"
+           '("hello *world*!")
+           '((:span "hello " (:em "world") "!")))
 
-  (testing "can parse text with strong"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("this is **very** important.") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:span "this is " (:strong "very") " important."))))))
+  (test-md "can parse text with strong"
+           '("this is **very** important.")
+           '((:span "this is " (:strong "very") " important.")))
 
-  (testing "can parse text with inline code"
-    (let ((output nil))
-      (md-parser:parse-markdown-lines '("`var` or ``var2``") (lambda (o) (push o output)))
-      (ok (equal (reverse output) '((:span (:code "var") " or " (:code "var2")))))))
+  (test-md "can parse text with inline code"
+           '("`var` or ``var2``")
+           '((:span (:code "var") " or " (:code "var2"))))
 
 
 )
