2afd08cbd49b301c1301bad05e0d4eb816fbac5c
Renato Athaydes <renato@athaydes.com>
2023-06-21 22:30:05 +0200

    Added condition/restart when parsing unmatched-symbol.

--- src/parse-md-span.lisp
+++ src/parse-md-span.lisp
@@ -1,8 +1,18 @@
 (defpackage md-span-parser
   (:use :cl)
-  (:export #:parse-md-span))
+  (:export #:parse-md-span
+           #:unmatched-symbol
+           #:unmatched-symbol-get #:unmatched-symbol-line #:unmatched-symbol-index))
 
 (in-package :md-span-parser)
+
+(define-condition unmatched-symbol (simple-warning)
+  ((symbol :initarg :c
+           :reader unmatched-symbol-get)
+   (line :initarg :line
+         :reader unmatched-symbol-line)
+   (index :initarg :index
+          :reader unmatched-symbol-index)))
 
 (defun go-up-to (c sym1 sym2 line index rcv)
   (let* ((two-chars? (and
@@ -14,9 +24,14 @@
                         (position c line :start (1+ index)))))
     (if maybe-end
         (funcall rcv (list (if two-chars? sym2 sym1) (subseq line (+ index adder) maybe-end)))
-        (funcall rcv (subseq line index)))
+        (restart-case (signal 'unmatched-symbol :c (if two-chars? sym2 sym1) :line line :index index)
+          (replace-value (value)
+            (funcall rcv (list value)))
+          (emit-raw-text ()
+            (funcall rcv (subseq line index)))
+          (do-nothing ()
+            nil)))
     (if maybe-end (+ maybe-end adder) (length line))))
-
 
 (defun emit-then-go-to (c sym1 sym2 line prev-index index rcv)
   (when (< prev-index index)
